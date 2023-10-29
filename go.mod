69d2ff4e3d5918dc5ba3c3ed5daa6a88bae8f63c
Oluwatunmise-olat <oluwatunmiseolatunbosun2001@gmail.com>
2023-10-29 01:35:36 +0100

    stream: logs to papertrail

--- go.mod
+++ go.mod
@@ -10,5 +10,12 @@ require (
 
 require (
 	github.com/joho/godotenv v1.5.1 // indirect
-	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	github.com/kdar/factorlog v0.0.0-20211012144011-6ea75a169038 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
+	github.com/polds/logrus-papertrail-hook v0.0.0-20180214143432-bcfe7b72c1a4 // indirect
+	github.com/sebest/logrusly v0.0.0-20180315190218-3235eccb8edc // indirect
+	github.com/segmentio/go-loggly v0.5.0 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
 )
--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,18 +1,38 @@
 package logger
 
 import (
+	"github.com/joho/godotenv"
+	logrus_papertrail "github.com/polds/logrus-papertrail-hook"
 	"github.com/sirupsen/logrus"
 	"os"
+	"strconv"
 )
 
 var (
-	Log = &logrus.Logger{
-		Out: os.Stdout,
-		Formatter: &logrus.JSONFormatter{
-			TimestampFormat: "2006-01-02 15:04:05",
-			PrettyPrint:     true,
-		},
-		Level:        logrus.InfoLevel,
-		ReportCaller: false,
-	}
+	Log *logrus.Logger
 )
+
+func init() {
+	godotenv.Load()
+
+	Log = logrus.New()
+	Log.Out = os.Stdout
+	Log.Formatter = &logrus.JSONFormatter{
+		TimestampFormat: "2006-01-02 15:04:05",
+		PrettyPrint:     true,
+	}
+	Log.Level = logrus.InfoLevel
+	Log.ReportCaller = false
+
+	port, _ := strconv.Atoi(os.Getenv("PAPERTRAIL_PORT"))
+	hook, _ := logrus_papertrail.NewPapertrailHook(&logrus_papertrail.Hook{
+		Host:    os.Getenv("PAPERTRAIL_HOST"),
+		Port:    port,
+		Appname: "sally-api-gateway",
+	})
+
+	hook.SetLevels([]logrus.Level{logrus.ErrorLevel, logrus.WarnLevel, logrus.InfoLevel})
+
+	Log.AddHook(hook)
+
+}
