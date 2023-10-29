daf679840b19b4a7fd7e7af8f74173ce838d82b0
Oluwatunmise-olat <oluwatunmiseolatunbosun2001@gmail.com>
2023-10-29 01:54:15 +0100

    Chore: clean up

--- main.go
+++ main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"github.com/Oluwatunmise-olat/custom-api-gateway/pkg/gateway"
-	"github.com/Oluwatunmise-olat/custom-api-gateway/pkg/logger"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load()
-
-	logger.Log.Infoln("Is New Version Deployed")
-
 	gateway.Bootstrap()
 }
